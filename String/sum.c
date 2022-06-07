90bf6dfa6e7e14daae59cf04d248a0891c05f9e1
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-07 05:50:14 +0000

    fix sum problem

--- String/sum.c
+++ String/sum.c
@@ -18,25 +18,44 @@ char *strrev(char *str)
 
 int main(void)
 {
-    char num1[100], num2[100], sum[100];
+    char num1[100], num2[100], sum[101];
     scanf("%s %s", num1, num2);
     strrev(num1);
     // reverse number string
     strrev(num2);
     int i = 0, j = 0, k = 0, l = 0;
     // intial loop variable
-    printf("%s %s\n", num1, num2);
     while (i < strlen(num1) && j < strlen(num2))
     {
         // loop run until one of number ends
         int add = ((num1[i] - '0') + (num2[j] - '0') + k) % 10;
         k = ((num1[i] - '0') + (num2[j] - '0')) / 10;
-        printf("%d\n", add);
         sum[l] = add + '0';
         i++;
         j++;
         l++;
     }
+    while (i < strlen(num1))
+    {
+        int add = ((num1[i] - '0') + k);
+        sum[l] = (add % 10) + '0';
+        k = add / 10;
+        i++;
+        l++;
+    }
+    while (j < strlen(num2))
+    {
+        int add = ((num2[j] - '0') + k);
+        sum[l] = (add % 10) + '0';
+        k = add / 10;
+        j++;
+        l++;
+    }
+    if (k > 0)
+    {
+        sum[l] = k + '0';
+    }
+
     strrev(sum);
     printf("%s\n", sum);
 }
