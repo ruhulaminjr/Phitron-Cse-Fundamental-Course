d22027a468309ce5d4bc5229887fe88f3649b752
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-10 17:18:15 +0000

    problem g solved

--- lab-exam-08/problem-f.c
+++ lab-exam-08/problem-f.c
@@ -3,4 +3,49 @@
 
 int main(void)
 {
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        char ss[100];
+        scanf("%s", ss);
+        if (n < 11)
+        {
+            printf("NO\n");
+        }
+        else if (n > 11)
+        {
+            int check = 0;
+            int del = n - 11;
+            for (int i = del; i >= 0; i--)
+            {
+                if (ss[i] == '8')
+                {
+                    check = 1;
+                    break;
+                }
+            }
+            if (check)
+            {
+                printf("YES\n");
+            }
+            else
+            {
+                printf("NO\n");
+            }
+        }
+        else
+        {
+            if (ss[0] == '8')
+            {
+                printf("YES\n");
+            }
+            else
+            {
+                printf("NO\n");
+            }
+        }
+    }
 }
