77c42000ef20c7d884315cc1c2c377ad56e3a9df
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-13 10:17:09 +0000

    array pointer

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -6,10 +6,23 @@ int main(void)
     // printf("Address of a >> %p Value of a is %d\n", &a, a);
     // int *p = &a;
     // printf("Address of p >> %p Value of p is %p\n", &p, p);
-    int c = 362436;
-    int *p = &c;
-    int **q = &p;
-    printf("Value of C -> %d Address of C -> %p\n",c,&c);
-    printf("Value of p -> %p Address of p -> %p\n",p,&p);
-    printf("Value of q -> %p Address of q -> %p \n",q,&q);
+    // int c = 362436;
+    // int *p = &c;
+    // int **q = &p;
+    // printf("Value of C -> %d Address of C -> %p\n",c,&c);
+    // printf("Value of p -> %p Address of p -> %p\n",p,&p);
+    // printf("Value of q -> %p Address of q -> %p \n",q,&q);
+    // printf("Value of q after dereferencing %p and value of p is %p value of c is %d \n", *q, p, **q);
+    int arr[5] = {10, 20, 30, 40, 50};
+    *(arr + 2) = -33;
+    // printf("%d \n",arr[2]);
+    int *ptr = arr;
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", *ptr);
+        // arr = arr +1;
+        // arr is constant pointer
+        *ptr++;
+    }
+    printf("\n");
 }
