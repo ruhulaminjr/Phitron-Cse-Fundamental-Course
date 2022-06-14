7384079a6ca47a56c8be9c037b6a87b90ab7cc11
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-14 05:00:49 +0000

    problem 2 done

--- practice-33/problem-2.c
+++ practice-33/problem-2.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
+int get_min(int a, int b)
+{
+    return a < b ? a : b;
+}
 int main(void)
 {
-    
+    int min = get_min(55, 66);
+    printf("%d\n", min);
 }
+/*
+Write a function named get_min() which will take two integer values as parameters and return the minimum of them
+*/
