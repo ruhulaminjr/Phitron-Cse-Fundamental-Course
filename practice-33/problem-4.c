33926e578d648dbff165337041df2469b580ad99
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-14 07:04:43 +0000

    problem 4 bug fixed

--- practice-33/problem-4.c
+++ practice-33/problem-4.c
@@ -12,21 +12,19 @@ void string_copy(char a[], char b[]);
 int main(void)
 {
     char a[100] = "My name is : ";
-    char b[] = "Ruhul Amin";
+    char b[] = "My Name is Ruhul Amin";
     string_copy(a, b);
     printf("%s\n", a);
 }
 void string_copy(char a[], char b[])
 {
-    int lena = get_leng(a);
     int lenb = get_leng(b);
-    int j = lena;
-    for (int i = 0 ; i < lenb; i++)
+    int i;
+    for (i=0; i < lenb; i++)
     {
-        a[j] = b[i];
-        j++;
+        a[i] = b[i];
     }
-    a[j] = '\0';
+    a[i] = '\0';
 }
 /*
 Write a function named string_copy() which will take two strings A and B as parameters and make copy of B into A without using any library function i.e. strcpy
