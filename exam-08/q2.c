ac5cebb4c1993661ee7ceb1d83e132413bad1473
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-18 03:51:14 +0000

    q2 answerd

--- exam-08/q2.c
+++ exam-08/q2.c
@@ -1,5 +1,23 @@
+
 #include <stdio.h>
+
+int make_average(int *arr, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    int average = sum / n;
+    return average;
+}
 
 int main(void)
 {
+    int num[] = {5, 10, 15};
+    printf("%d \n", make_average(num, 3));
 }
+
+/*
+Make a function named make_average() which will take an array of integers and the size of that array and return the average of those values.
+*/
