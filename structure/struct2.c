149870bda6428af6d5232480835b2903e176f40a
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-19 10:11:12 +0000

    find the best batsman using structure

---
+++ structure/struct2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct Cricketer
+{
+    char name[100];
+    int score;
+    int age;
+};
+int main(void)
+{
+    struct Cricketer batsman;
+    struct Cricketer best;
+    printf("Enter Batsman Details: ");
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%s %d %d", batsman.name, &batsman.score, &batsman.age);
+        printf("Name : %s Score : %d Age: %d \n", batsman.name, batsman.score, batsman.age);
+        if (batsman.score > best.score)
+        {
+            best = batsman;
+        }
+    }
+    printf("Best Batsman is Name : %s Score : %d \n", best.name, best.score);
+}
