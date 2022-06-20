2ac1af2607f1dc750a9ad6b44fefc44846df8636
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-20 09:29:10 +0000

    input data from file and make sum

---
+++ file/inputfile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+    FILE *inputfile = fopen("input.txt", "r");
+    FILE *outputfile = fopen("output.txt", "w");
+    if (inputfile == NULL)
+    {
+        fprintf(outputfile, "No Input file found\n");
+        return 0;
+    }
+    int n;
+    fscanf(inputfile, "%d", &n);
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        fscanf(inputfile, "%d", &a);
+        sum += a;
+    }
+    fprintf(outputfile, "Sum -> %d\n", sum);
+}
