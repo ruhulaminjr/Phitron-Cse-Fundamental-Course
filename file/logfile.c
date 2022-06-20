d689e94aa9d338b4a064e15ff102a7443dd5129d
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-20 09:51:30 +0000

    add log file

---
+++ file/logfile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *logfile = fopen("logfile.txt", "a");
+    FILE *infile = fopen("infile.txt", "r");
+    if (infile == NULL)
+    {
+        fprintf(logfile, "File Not found at 15:51:11 \n");
+        return 0;
+    }
+}
