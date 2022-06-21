f49196e98aadd1e38fb801f277158ad3b7e3b5b1
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-21 14:18:10 +0000

    problem 2 file read and make sum

---
+++ practice-37/problem-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int sum = 0;
+    int a, b;
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum = %d\n", sum);
+}
+/*
+Write a C program to calculate the sum of two integers. Make a file named “in.txt” and take input from that file using command prompt / terminal and show output in that command prompt.
+*/
---
+++ practice-37/problem-3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+}
+/*
+Write a C program to calculate the sum of two integers. Make a file named “out.txt” and get output in that file using command prompt / terminal and take input in that command prompt.
+*/
