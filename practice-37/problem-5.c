7b37e3c80f331b2aa0a5cd2187c25e87f749ae46
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-21 14:34:33 +0000

    character count from input.txt file

---
+++ practice-37/problem-5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *inputfile = fopen("input.txt", "r");
+    if (inputfile == NULL)
+    {
+        perror("Input File Not Found!");
+        return 0;
+    }
+    int count = 0;
+    while (1)
+    {
+        char ch = fgetc(inputfile);
+        if (ch == EOF)
+        {
+            break;
+        }
+        printf("%c", ch);
+        if(ch != ' '){
+            count++;
+        }
+        
+    };
+    printf("\ncharacter found : %d\n", count);
+    fclose(inputfile);
+}
