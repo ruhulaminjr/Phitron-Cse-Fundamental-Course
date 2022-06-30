d734ce4934af86f9d46a4f11f18abe1b22225eb1
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-06-30 12:09:19 +0000

    question 9

---
+++ course-2-final-exam/written/q-9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct Student
+{
+    int roll;
+    int class;
+    int marks;
+};
+void input(struct Student *st)
+{
+    scanf("%d %d %d", &st->roll, &st->class, &st->marks);
+}
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+    struct Student arr[n];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        input(&arr[i]);
+        sum += arr[i].marks;
+    }
+    printf("%d\n",sum);
+}
