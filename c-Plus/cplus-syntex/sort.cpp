082e31eae5350bb909d8d45590456a40fd1b807c
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-07-07 02:59:11 +0000

    cpp a problem solve

--- c-Plus/cplus-syntex/sort.cpp
+++ c-Plus/cplus-syntex/sort.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void)
 {
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
---
+++ c-Plus/practice-2/problem-a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int x, t;
+    cin >> x >> t;
+    if (x > t)
+    {
+        cout << x - t << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
+}
+/*
+We have a sandglass that runs for XX seconds. The sand drops from the upper bulb at a rate of 11 gram per second. That is, the upper bulb initially contains XX grams of sand.
+
+How many grams of sand will the upper bulb contains after tt seconds?
+*/
