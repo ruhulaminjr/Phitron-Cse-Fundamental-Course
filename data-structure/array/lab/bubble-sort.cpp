c0fb9d8e72d5a3896a5384b80543fa611caa8abe
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-07-29 05:00:47 +0000

    optimize bubble sort

--- data-structure/array/lab/bubble-sort.cpp
+++ data-structure/array/lab/bubble-sort.cpp
@@ -23,15 +23,22 @@ int main(void)
     // bubble sor implementation
     for (int i = 0; i < size; i++)
     {
+        int flag = 0;
+        cout << "iteration Number : " << i + 1 << endl;
         for (int j = 0; j < size - 1 - i; j++)
         {
-            if (arr[j] < arr[j + 1])
+            if (arr[j] > arr[j + 1])
             {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                flag = 1;
             }
+            printArray(arr, size);
         }
+        cout << endl;
+        if (flag == 0)
+            break;
     }
     cout << "After sort : ";
     printArray(arr, size);
