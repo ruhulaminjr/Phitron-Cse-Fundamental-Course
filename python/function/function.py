d4d0696442a856fb35e31b6e6f4f8f8f98ae9e47
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-10-09 11:34:56 +0000

    python practice

--- python/function/function.py
+++ python/function/function.py
@@ -2,13 +2,9 @@ def add(num1, num2=55):  # num5):
     # we cannot add normal parameter after add optional parameter
     sum = num1 + num2
     return sum
-
-
 result = add(15)
 
 # print(result)
-
-
 def multiply(*numbers):
     # print(numbers)
     # (44, 23, 23, 1, 2)
@@ -16,23 +12,14 @@ def multiply(*numbers):
     for i in numbers:
         result *= i
     return result
-
-
 res = multiply(44, 23, 23, 1, 2)
 
 print(res)
-
-
 def full_name(fname, lname):
     name = f'{fname} {lname}'
     return name
-
-
 fullName = full_name(lname="Amin", fname="Ruhul")
-
 print(fullName)
-
-
 def all_types(name, *hobby, **parents):
     print(f'Name : {name}')
     print(hobby)
@@ -45,3 +32,19 @@ def all_types(name, *hobby, **parents):
 
 all_types("Super Hero", "Fight", "Help People",
           "Save Earth From Evil", father="Bat-Man", mother="Cat-Women")
+
+balance = 580
+
+def total_cost(price,qty):
+    global balance
+    cost = price * qty
+    balance-= cost
+    return cost
+
+print(f"Total Balance : Before {balance}")
+
+price = total_cost(15,9)
+
+print(f"Total Price {price} ")
+
+print(f"Total Balance After {balance}")
---
+++ python/function/modulus.py
@@ -0,0 +1,8 @@
+# import function
+# from function import add
+from function import *
+
+
+sum = add(55, 12)
+
+print(f'sum is modulus {sum}')
