e1a7ac7cb078e9cec38dd60a5100ca2f8d35b7cf
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-11-06 09:36:42 +0000

    creating aircraft class

---
+++ python/oop/flight-scheduler/air_lines.py
@@ -0,0 +1,22 @@
+from Aircraft import Aircraft
+import csv
+
+
+class all_airCraft:
+    def __init__(self):
+        self.all_aircraft = None
+        self.load_aircraft("./data/aircraft.csv")
+
+    def load_aircraft(self, csv_file):
+        all_air_craft = {}
+        with open(csv_file, 'r') as file:
+            lines = csv.reader(file)
+            next(lines)
+            for line in lines:
+                all_air_craft[line[0]] = Aircraft(
+                    make=line[3], code=line[0], typ=line[2], flight_range=line[4])
+            file.close()
+        self.all_aircraft = all_air_craft
+
+
+all_airCraft()
