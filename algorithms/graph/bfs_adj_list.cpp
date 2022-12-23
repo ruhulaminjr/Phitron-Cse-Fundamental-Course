c0ef958617ee9552dcfd4fc572be234f0c29ef23
Ruhul Amin <76563477+ruhulaminjr@users.noreply.github.com>
2022-12-23 06:28:19 +0000

    bfs and dfs implemt in adj list

---
+++ algorithms/graph/bfs_adj_list.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5;
+int visited[N] = {};
+vector<int> adj_list[N];
+
+/*
+Nodes = 6 ,Edge=6;
+0--1-----2-------4--------6
+         |       |
+         |       |
+         5-------3
+6 7
+0 1
+2 4
+2 1
+5 2
+3 5
+4 3
+4 6
+
+output : 0 1 2 4 5 3 6
+
+*/
+/*
+step 1 : initilize a visited array to check element visited
+step 2 : use q data structure to store node
+step 3 : run loop until q empty and push all adjacency node into the q;
+
+*/
+
+void bfs(int node)
+{
+
+    queue<int> q;
+    q.push(node);
+    visited[node] = 1;
+    while (!q.empty())
+    {
+        int head = q.front();
+        q.pop();
+        cout << head<< " ";
+        for (int adjNode : adj_list[head])
+        {
+            if (visited[adjNode] == 0)
+            {
+                visited[adjNode] = 1;
+                q.push(adjNode);
+            }
+        }
+    }
+}
+int main(void)
+{
+    int nodes, edges;
+    cin >> nodes >> edges;
+    for (int i = 0; i < edges; i++)
+    {
+        int u, v;
+        cin>>u>>v;
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+    bfs(0);
+    cout<<endl;
+}
