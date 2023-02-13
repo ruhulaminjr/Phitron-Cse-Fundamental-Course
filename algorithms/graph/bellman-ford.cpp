6b0fca25fc6e3a0a8f68453daf308da9450e081e
ruhulaminjr <ruhultuber@gmail.com>
2023-02-13 20:45:59 +0600

    daily push code

--- algorithms/graph/bellman-ford.cpp
+++ algorithms/graph/bellman-ford.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-const int INF = 2e9;
+const long long INF = 1e18;
 const int N = 1e5;
-int d[N];
+long long d[N];
+int parent[N];
 vector<pair<int, int>> adj_list[N];
 int main(void)
 {
@@ -16,11 +16,14 @@ int main(void)
     for (int i = 0; i < m; i++)
     {
         int u, v, w;
+        cin >> u >> v >> w;
         adj_list[u].push_back({v, w});
         adj_list[v].push_back({u, w});
     }
     // BELLMAN-FORM ALLGORITHMS
-    for (int i = 1; i <= n - 1; i++)
+    bool negativeCycle = false;
+    int last_update_node = -1;
+    for (int i = 1; i <= n; i++)
     {
         for (int node = 1; node <= n; node++)
         {
@@ -32,14 +35,44 @@ int main(void)
                 if (d[u] + w < d[v])
                 {
                     d[v] = d[u] + w;
+                    parent[v] = u;
+                    if (i == n)
+                    {
+                        negativeCycle = true;
+                        last_update_node = v;
+                    }
                 }
             }
         }
     }
-
-    for (int i = 1; i <= n; i++)
+    if (negativeCycle == false)
     {
-        cout << "Node: " << i << "Cost: " << d[i] << " ";
+        cout << "NO" << endl;
+        return 0;
+    }
+    cout << "YES" << endl;
+    int selectedNode = last_update_node;
+    for (int i = 1; i <= n - 1; i++)
+    {
+        selectedNode = parent[selectedNode];
+    }
+    int currNode = selectedNode;
+    vector<int> cycle;
+    cycle.push_back(selectedNode);
+    while (true)
+    {
+        selectedNode = parent[selectedNode];
+        cycle.push_back(selectedNode);
+        if (selectedNode == currNode)
+        {
+            break;
+        }
+    }
+    reverse(cycle.begin(), cycle.end());
+    for (int node : cycle)
+    {
+        cout << node << " ";
     }
     cout << endl;
+    return 0;
 }
